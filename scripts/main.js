548e220e3879101d05e4855807f85d0de93e4d48
Paul Nitto <pnitto18@gmail.com>
2015-09-09 20:49:33 -0400

    progress bar, time and background color changing

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
- function timePerSecond() {
-  var timePast = 1000;
-  var time = setTimeout('clock()', timePast);
-  //console.log(time);
+
+function timePerSecond() {
+ var timePast = 1000;
+ var time = setTimeout('clock()', timePast);
+ //console.log(time);
 }
+
 function clock() {
   var clock = new Date();
   var seconds = clock.getSeconds();
@@ -11,26 +13,29 @@ function clock() {
   //console.log(minutes);
   var hours = clock.getHours();
   //console.log(hours);
-  //if(hours <= 9) hr = '0'+ hours;
-	//if(minutes <= 9) ms = '0'+ minutes;
-	//if(seconds <= 9) s = '0'+ seconds;
+  if(hours <= 9) hours = '0'+ hours;
+  if(minutes <= 9) minutes = '0'+ minutes;
+  if(seconds <= 9) seconds = '0'+ seconds;
+  var color = "#" + hours + minutes + seconds;
+  document.body.style.background = color;
+  document.getElementById('body').innerHTML = color;
 
-  //color = "#" + hours + minutes + seconds;
-  //document.body.style.background = color;
-  //document.getElementById('body').innerHTML = color;
   var everySecond = timePerSecond();
-
   var secondPercent = Math.round((seconds / 60) * 100);
+  console.log(secondPercent);
 
   var progressbar = document.querySelector('#progress-bar');
+  console.log(progressbar);
   progressbar.style.width = secondPercent + '%';
 
   document.querySelector('.js-second-percent').textContent = secondPercent;
+  document.querySelector('.js-clock').textContent = clock;
+
   //console.log(secondPercent);
   //console.log(seconds);
   //console.log(clock);
-  document.querySelector('.js-clock').textContent = clock;
 }
+
 
 
 
